03a1ca7031d2b9667bd611b364c647db6fd5ba66
Keginold Philip <keginoldp@hotmail.com>
2021-01-13 18:53:02 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(string)
   if string != string.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif string.upcase
+  elsif string == string.upcase
     return "NO, NOT SINCE 1938!"
   else string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
